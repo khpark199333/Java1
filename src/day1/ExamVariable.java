f897eebb8ed25ca922d99a85ba1623ffaba060de
khpark199333 <khpark199333@naver.com>
2020-04-17 16:51:53 +0900

    1일차변수예제

---
+++ src/day1/ExamVariable.java
@@ -0,0 +1,20 @@
+package day1;
+
+import java.util.Scanner;
+
+public class ExamVariable {
+        // TODO Auto-generated method stub
+		public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		System.out.println(num);
+		char ch = scan.next().charAt(0);
+		System.out.println(ch);
+		//문자열을 관리하는 String 클래스
+		String str = new string();
+		str = scan.next();
+		System.out.println(str); 
+		scan.close();
+	}
+
+}
