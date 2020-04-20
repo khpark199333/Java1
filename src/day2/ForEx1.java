7bec121583dcd8e33e9d65f7b17152f58cc20c2d
khpark199333 <khpark199333@naver.com>
2020-04-20 17:35:51 +0900

    풔예제1

---
+++ src/day2/ForEx1.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		// for문 기본 예제 : Hello world 5번 출력
+		int i;
+		for(i=1; i<=5; i++) {
+			System.out.println("Hello world");
+		}
+		for(i=0; i<=5; i++) {
+			System.out.println("Hello world");
+		}
+	}
+
+}
