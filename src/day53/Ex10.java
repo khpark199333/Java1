6f8d9cad72374b417fa4c4a8fb1f24bb854a3d4f
khpark199333 <khpark199333@naver.com>
2020-07-06 17:02:19 +0900

    day53ex10

---
+++ src/day53/Ex10.java
@@ -0,0 +1,15 @@
+package day53;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static double sum(double num1, double num2) {
+		return num1 + num2;
+	}
+	public static double sum(double num1, double num2,double num3) {
+		return sum(num1,num2) + num3;
+	}
+}
