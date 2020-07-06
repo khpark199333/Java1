1ee3e431288374ab1754997ec1e3febcaa872648
khpark199333 <khpark199333@naver.com>
2020-07-06 17:01:36 +0900

    day53ex5

---
+++ src/day53/ex5.java
@@ -0,0 +1,16 @@
+package day53;
+
+public class ex5 {
+
+	public static void main(String[] args) {
+		//  구구단예제 
+		int num =9;
+		int i =1;
+		
+		while(i <=9) {
+				 System.out.printf("%d x %d = %d\n", num, i, num*i);
+				 i++;
+		}		
+	}
+
+}
